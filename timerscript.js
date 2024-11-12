f70f3f63b886632a115d2596fc137a2d06eb592a
Niccolò Albanese <nick_albanese@virgilio.it>
2024-11-12 16:05:32 +0100

    done

--- timerscript.js
+++ timerscript.js
@@ -1,15 +1,22 @@
 let counter = 0;
-let key = sessionStorage.setItem("counter", counter++)
 
-function incrementCounter() {
-    counter++;
-    document.getElementById("clock").textContent = counter; // Aggiorna anche l’orologio
-}
 
 if (sessionStorage.getItem("counter")) {
-    counter = parseInt(sessionStorage.getItem("counter"));
+
+  counter = parseInt(sessionStorage.getItem("counter"))
 } else {
-    counter = 0;
+
+  counter = 0;
 }
 
-setInterval(incrementCounter, 1000);
+
+document.getElementById("clock").textContent = counter
+
+function incrementCounter() {
+  counter++
+  document.getElementById("clock").textContent = counter
+  sessionStorage.setItem("counter", counter)
+}
+
+
+setInterval(incrementCounter, 1000)
